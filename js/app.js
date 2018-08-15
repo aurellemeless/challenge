9053ce7845125ce3a45e708be46d069eb0506800
Aurelle Meless <aurelle.meless@gmail.com>
2018-08-15 12:21:22 +0000

    localStorage Added, UI improved

--- js/app.js
+++ js/app.js
@@ -1,17 +1,33 @@
+
 // initialize angular chalenge module
 angular.module("challenge", ["ngResource"]);
 
+ if(localStorage.getItem('database')==null){
+    localStorage.setItem('database', JSON.stringify([]));
+ }
+
 angular.module('challenge')
     .controller('ChallengeController',function($scope, $filter){
-        // var database = [];
-        
         $scope.user = {};
         $scope.onlyMinor = false;
-        $scope.userList = [];
+        // get list from database
+        $scope.userList = JSON.parse(localStorage.getItem('database'));
         $scope.save = function(){
             var newUser = $scope.user;
-            // add user to the list
-            $scope.userList.push({
+            database = [];
+            // remake list to avoid ngrepeat index issue
+            angular.forEach($scope.userList, function(i){
+                database.push({
+                    firstname : i.firstname,
+                    lastname : i.lastname,
+                    age : i.age,
+                    sexe : i.sexe,
+                    city : i.city,
+                    country : i.country
+                });
+            });
+            // add the form data as new user
+            database.push({
                 firstname : newUser.firstname,
                 lastname : newUser.lastname,
                 age : newUser.age,
@@ -19,6 +35,9 @@ angular.module('challenge')
                 city : newUser.city,
                 country : newUser.country
             });
+            $scope.userList = database;
+            // store in database
+            localStorage.setItem('database', JSON.stringify(database));
             // reset the form
                $scope.user.firstname='';
                 $scope.user.lastname='';
@@ -26,6 +45,7 @@ angular.module('challenge')
                $scope.user.sexe='male';
                 $scope.user.city='Abidjan';
                 $scope.user.country='Belgium';
+            
         };
 
 
